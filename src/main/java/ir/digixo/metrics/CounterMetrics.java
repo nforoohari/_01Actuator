8ef3a81e248b912ea40bfcb7297397ddfbf9f90f
Nima Foroohari <nforoohari@gmail.com>
2025-05-14 11:17:26 +0330

    Version 5

--- src/main/java/ir/digixo/metrics/CounterMetrics.java
+++ src/main/java/ir/digixo/metrics/CounterMetrics.java
@@ -1,31 +1,28 @@
-//package ir.digixo.metrics;
-//
-//import io.micrometer.core.instrument.Counter;
-//import io.micrometer.core.instrument.Gauge;
-//import io.micrometer.core.instrument.MeterRegistry;
-//import io.micrometer.core.instrument.Timer;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//
-//@RestController
-//@RequestMapping("/")
-//public class CounterMetrics {
-//
-//
-//    Counter counter;
-//
-//    public CounterMetrics(MeterRegistry registry) {
-//        counter =Counter.builder("MyCounter22")
-//                .description("My counter")
-//                .register(registry);
-//    }
-//
-//    @GetMapping("/m")
-//    public String m()
-//    {
-//        counter.increment();
-//        return "hello rest";
-//    }
-//}
+package ir.digixo.metrics;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.Timer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class CounterMetrics {
+
+    Counter counter;
+
+    public CounterMetrics(MeterRegistry registry) {
+        counter = Counter.builder("Nima Counter")
+                .description("Nima Counter Description")
+                .register(registry);
+    }
+
+    @GetMapping("/c")
+    public String c() {
+        counter.increment();
+        return "Hello, Counter Incremented";
+    }
+}
--- src/main/java/ir/digixo/metrics/InfoMetrics.java
+++ src/main/java/ir/digixo/metrics/InfoMetrics.java
@@ -1,22 +1,23 @@
-//package ir.digixo.metrics;
-//
-//import org.springframework.boot.actuate.health.Health;
-//import org.springframework.boot.actuate.health.HealthIndicator;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//public class InfoMetrics implements HealthIndicator {
-//    @Override
-//    public Health getHealth(boolean includeDetails) {
-//        return HealthIndicator.super.getHealth(includeDetails);
-//    }
-//
-//    @Override
-//    public Health health() {
-//        boolean error = false;
-//        if (error) {
-//            return Health.down().withDetail("error code", 800).build();
-//        }
-//        return Health.up().build();
-//    }
-//}
+package ir.digixo.metrics;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class InfoMetrics implements HealthIndicator {
+
+    @Override
+    public Health getHealth(boolean includeDetails) {
+        return HealthIndicator.super.getHealth(includeDetails);
+    }
+
+    @Override
+    public Health health() {
+        boolean error = true;
+        if (error) {
+            return Health.down().withDetail("Error Code", 800).build();
+        }
+        return Health.up().withDetail("Status Code", 200).build();
+    }
+}
